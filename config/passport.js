958edee460808471895cb8051f5cd943f7752e4b
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-05-13 18:20:08 -0500

    Fixed passport refactor

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,29 @@ const User = require('../models/User')
 
 module.exports = function (passport) {
   
-    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-    try {
-        const user = await User.findOne({ email: email.toLowerCase() }); 
+    passport.use(
+      new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        try {
+          const user = await User.findOne({ email: email.toLowerCase() });
+          if (!user) {
+            return done(null, false, { msg: `Email ${email} not found.` })
+          }
+          if (!user.password) {
+            return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
+          }
+          user.comparePassword(password, (err, isMatch) => {
+            if (err) { return done(err) }
+            if (isMatch) {
+              return done(null, user)
+            }
+            return done(null, false, { msg: 'Invalid email or password.' })
+          });
     } catch (err) {
         return done(err);
     }
-    if (!user) {
-      return done(null, false, { msg: `Email ${email} not found.` })
-    }
-    if (!user.password) {
-      return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
-    }
-    user.comparePassword(password, (err, isMatch) => {
-      if (err) { return done(err) }
-      if (isMatch) {
-        return done(null, user)
-      }
-      return done(null, false, { msg: 'Invalid email or password.' })
-    });
-    }));
+    })
+  );
     
-
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 
  exports.getLogin = (req, res) => {
     if (req.user) {
-      return res.redirect('/todos')
+      return res.redirect('/')
     }
     res.render('login', {
       title: 'Login'
@@ -57,35 +57,42 @@ const User = require('../models/User')
   }
   
   exports.postSignup = async (req, res, next) => {
-    const validationErrors = [];
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' });
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' });
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' });
-  
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors);
-      return res.redirect('/signup');
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
-  
-    const user = new User({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password
-    });
-  
     try {
+      const validationErrors = [];
+      if (!validator.isEmail(req.body.email))
+        validationErrors.push({ msg: "Please enter a valid email address." });
+      if (!validator.isLength(req.body.password, { min: 8 }))
+        validationErrors.push({
+          msg: "Password must be at least 8 characters long"
+        });
+      if (req.body.password !== req.body.confirmPassword)
+        validationErrors.push({ msg: "Passwords do not match" });
+  
+      if (validationErrors.length) {
+        req.flash("errors", validationErrors);
+        return res.redirect("../signup");
+      }
+  
+      req.body.email = validator.normalizeEmail(req.body.email, {
+        gmail_remove_dots: false
+      });
+  
       const existingUser = await User.findOne({
-        $or: [
-          { email: req.body.email },
-          { userName: req.body.userName }
-        ]
+        $or: [{ email: req.body.email }, { userName: req.body.userName }]
       });
   
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' });
-        return res.redirect('/signup');
+        req.flash("errors", {
+          msg: "Account with that email address or username already exists."
+        });
+        return res.redirect("../signup");
       }
+  
+      const user = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password
+      });
   
       await user.save();
   
@@ -93,7 +100,7 @@ const User = require('../models/User')
         if (err) {
           return next(err);
         }
-        res.redirect('/cart');
+        res.redirect("/profile");
       });
     } catch (err) {
       return next(err);
