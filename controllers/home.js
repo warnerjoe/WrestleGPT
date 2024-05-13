f67ce15cdadc8516415403430b7b4605375b5515
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-05-13 17:27:20 -0500

    Split EJS into partials, styled login / signups page to include nav

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
+    },
+    getMeltzerGPT: (req, res) => {
+        res.render('meltzergpt.ejs')
     }
 }
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
+router.get('/meltzergpt', homeController.getMeltzerGPT)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
