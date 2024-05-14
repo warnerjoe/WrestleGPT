3ecc9d0918be2cc7b6cf18ba21017222ec3eeceb
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-05-13 23:31:25 -0500

    Added admin role and privileges

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,11 @@
 module.exports = {
     getIndex: (req,res)=>{
-        res.render('index.ejs')
+        res.render('index', { user: req.user });
     },
     getMeltzerGPT: (req, res) => {
         res.render('meltzergpt.ejs')
+    },
+    getProfile: (req, res) => {
+        res.render('profile.ejs')
     }
 }
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,19 @@
 module.exports = {
-    ensureAuth: function (req, res, next) {
+  ensureAuth: function (req, res, next) {
       if (req.isAuthenticated()) {
-        return next()
+          return next();
       } else {
-        res.redirect('/')
+          res.redirect('/');
       }
+  },
+  adminAuth: function (req, res, next) {
+    // Check if user is authenticated and isAdmin is true
+    if (req.isAuthenticated() && req.user.isAdmin) {
+        // User is authenticated and is an admin, proceed to the next middleware/route handler
+        return next();
+    } else {
+        // User is not authenticated or is not an admin, redirect to the home page or display an error message
+        res.redirect('/');
     }
   }
-  
+};
