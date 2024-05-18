2957fbf190836e3a2b811384f43d10e2b26eed44
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-05-18 00:20:48 -0500

    Started constructing roster page

--- routes/main.js
+++ routes/main.js
@@ -4,9 +4,12 @@ const authController = require('../controllers/auth')
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest, adminAuth } = require('../middleware/auth')
 
+// Routes for different portions of website, /controllers/home.js
 router.get('/', homeController.getIndex)
 router.get('/meltzergpt', adminAuth, homeController.getMeltzerGPT)
 router.get('/profile', ensureAuth, homeController.getProfile)
+
+// Routes for User Authentication, /controllers/auth.js
 router.get('/roster', homeController.getRoster)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
